ce5b3ae483faba5571e071cb818e92914c6ad12b
johnrcd <80877409+johnrcd@users.noreply.github.com>
2024-08-30 21:01:18 -0500

    added cool glow effect

---
+++ bg.js
@@ -0,0 +1,8 @@
+addEventListener("DOMContentLoaded", (event) => {
+    Particles.init({
+        selector: '.background',
+        color: "#1d3243",
+        connectParticles: true,
+    });   
+});
+
